d11d7c0ce5216e53bd41faf3233e015498e4894a
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-17 13:19:34 +0200

    add hw 2 mongodb

---
+++ src/db/models/contacts.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+    },
+
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = mongoose.model('contacts', contactsSchema);
