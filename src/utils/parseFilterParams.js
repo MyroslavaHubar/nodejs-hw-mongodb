dce4543f1a71b81c8a20e3be55263f2b27f676a3
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-22 12:19:50 +0200

    add filter

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,27 @@
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isType = (contactType) =>
+    ['work', 'home', 'personal'].includes(contactType);
+
+  if (isType(contactType)) return contactType;
+};
+
+const parseContactIsFavourite = (value) => {
+  if (value === 'true' || value === 'false') {
+    return value;
+  }
+  return undefined;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parsedContactType = parseContactType(contactType);
+  const parsedContactIsFavourite = parseContactIsFavourite(isFavourite);
+
+  return {
+    contactType: parsedContactType,
+    isFavourite: parsedContactIsFavourite,
+  };
+};
