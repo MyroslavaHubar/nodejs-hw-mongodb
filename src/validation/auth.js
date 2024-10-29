7fa185e56db888cb86eb0f1d431c55b4c5b48292
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-29 22:05:10 +0100

    add login validation

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -14,3 +14,12 @@ export const registerUserSchema = Joi.object({
     'any.required': 'Password is required',
   }),
 });
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'any.required': 'Email is required',
+  }),
+  password: Joi.string().required().messages({
+    'any.required': 'Password is required',
+  }),
+});
